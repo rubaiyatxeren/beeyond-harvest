5cb9107dece37c6e04559c6a16639932aeee0f10
Rubaiyat <140968853+rubaiyatxeren@users.noreply.github.com>
2026-03-31 22:40:50 +0600

    Update authMiddleware.js

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -4,21 +4,25 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const protect = async (req, res, next) => {
+  let token;
+
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
     try {
-      const token = req.headers.authorization.split(" ")[1];
+      token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await Admin.findById(decoded.id).select("-password");
+      req.admin = await Admin.findById(decoded.id).select("-password");
       next();
     } catch (error) {
       res
         .status(401)
         .json({ success: false, message: "Not authorized, token failed" });
     }
-  } else {
+  }
+
+  if (!token) {
     res
       .status(401)
       .json({ success: false, message: "Not authorized, no token" });
